eb45013e95bdc94b2e9323428f0e757ede25ee4a
NateNBJ <nate.nbj@gmail.com>
2023-05-07 09:29:48 -0600

    Add files via upload

---
+++ src/new_beginnings/LunaSettingsChangedListener.java
@@ -0,0 +1,27 @@
+package new_beginnings;
+
+import com.fs.starfarer.api.Global;
+import lunalib.lunaSettings.LunaSettings;
+import lunalib.lunaSettings.LunaSettingsListener;
+
+import static new_beginnings.ModPlugin.LUNALIB_ID;
+
+public class LunaSettingsChangedListener implements LunaSettingsListener {
+    @Override
+    public void settingsChanged(String idOfModWithChangedSettings) {
+        if(idOfModWithChangedSettings.equals(ModPlugin.ID)) {
+            try {
+                ModPlugin.readSettings();
+            } catch (Exception e) {
+                ModPlugin.reportCrash(e);
+            }
+        }
+    }
+    public static void addToManagerIfNeeded() {
+        if(Global.getSettings().getModManager().isModEnabled(LUNALIB_ID)
+                && !LunaSettings.INSTANCE.hasListenerOfClass(LunaSettingsChangedListener.class)) {
+
+            LunaSettings.INSTANCE.addListener(new LunaSettingsChangedListener());
+        }
+    }
+}
--- src/new_beginnings/ModPlugin.java
+++ src/new_beginnings/ModPlugin.java
@@ -3,14 +3,54 @@ package new_beginnings;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.GameState;
 import com.fs.starfarer.api.Global;
+import lunalib.lunaSettings.LunaSettings;
 import new_beginnings.campaign.CampaignPlugin;
 import org.json.JSONObject;
 
 import java.awt.*;
+import java.util.MissingResourceException;
 
 public class ModPlugin extends BaseModPlugin {
     public static final String ID = "sun_new_beginnings";
+    public static final String PREFIX = "sun_nb_";
     public static final String SETTINGS_PATH = "NEW_BEGINNINGS_OPTIONS.ini";
+
+    static final String LUNALIB_ID = "lunalib";
+    static JSONObject settingsCfg = null;
+    static <T> T get(String id, Class<T> type) throws Exception {
+        if(Global.getSettings().getModManager().isModEnabled(LUNALIB_ID)) {
+            id = PREFIX + id;
+
+            if(type == Integer.class) return type.cast(LunaSettings.getInt(ModPlugin.ID, id));
+            if(type == Float.class) return type.cast(LunaSettings.getFloat(ModPlugin.ID, id));
+            if(type == Boolean.class) return type.cast(LunaSettings.getBoolean(ModPlugin.ID, id));
+            if(type == Double.class) return type.cast(LunaSettings.getDouble(ModPlugin.ID, id));
+            if(type == String.class) return type.cast(LunaSettings.getString(ModPlugin.ID, id));
+        } else {
+            if(settingsCfg == null) settingsCfg = Global.getSettings().getMergedJSONForMod(SETTINGS_PATH, ID);
+
+            if(type == Integer.class) return type.cast(settingsCfg.getInt(id));
+            if(type == Float.class) return type.cast((float) settingsCfg.getDouble(id));
+            if(type == Boolean.class) return type.cast(settingsCfg.getBoolean(id));
+            if(type == Double.class) return type.cast(settingsCfg.getDouble(id));
+            if(type == String.class) return type.cast(settingsCfg.getString(id));
+        }
+
+        throw new MissingResourceException("No setting found with id: " + id, type.getName(), id);
+    }
+    static int getInt(String id) throws Exception { return get(id, Integer.class); }
+    static double getDouble(String id) throws Exception { return get(id, Double.class); }
+    static float getFloat(String id) throws Exception { return get(id, Float.class); }
+    static boolean getBoolean(String id) throws Exception { return get(id, Boolean.class); }
+    static String getString(String id) throws Exception { return get(id, String.class); }
+    static boolean readSettings() throws Exception {
+        RESPAWN_FLEET_VALUE_MULT = getFloat("respawnFleetValueMult");
+        MAX_RESPAWN_FLEET_VALUE = getFloat("maxRespawnFleetValue");
+        MIN_RESPAWN_FLEET_VALUE = getFloat("minRespawnFleetValue");
+        CHANCE_TO_RESPAWN_WITH_NON_PLAYER_FACTION_FLEET_MULTIPLIER = getFloat("chanceToRespawnWithNonPlayerFactionFleetMultiplier");
+
+        return true;
+    }
 
     static boolean settingsAreRead = false;
 
@@ -22,12 +62,16 @@ public class ModPlugin extends BaseModPlugin {
 
     CampaignScript script;
 
+    void removeScripts() {
+        Global.getSector().removeTransientScript(script);
+        Global.getSector().removeListener(script);
+    }
+
     @Override
     public void beforeGameSave() {
         try {
             Saved.updatePersistentData();
-            Global.getSector().removeTransientScript(script);
-            Global.getSector().removeListener(script);
+            removeScripts();
         } catch (Exception e) { reportCrash(e); }
     }
 
@@ -43,10 +87,16 @@ public class ModPlugin extends BaseModPlugin {
     @Override
     public void onGameLoad(boolean newGame) {
         try {
+            removeScripts();
+
             Global.getSector().registerPlugin(new CampaignPlugin());
             Global.getSector().addTransientScript(script = new CampaignScript());
 
             Saved.loadPersistentData();
+
+            if(Global.getSettings().getModManager().isModEnabled(LUNALIB_ID)) {
+                LunaSettingsChangedListener.addToManagerIfNeeded();
+            }
 
             readSettingsIfNecessary();
         } catch (Exception e) { reportCrash(e); }
@@ -56,12 +106,7 @@ public class ModPlugin extends BaseModPlugin {
         try {
             if(settingsAreRead) return true;
 
-            JSONObject cfg = Global.getSettings().getMergedJSONForMod(SETTINGS_PATH, ID);
-
-            RESPAWN_FLEET_VALUE_MULT = (float) cfg.getDouble("respawnFleetValueMult");
-            MAX_RESPAWN_FLEET_VALUE = (float) cfg.getDouble("maxRespawnFleetValue");
-            MIN_RESPAWN_FLEET_VALUE = (float) cfg.getDouble("minRespawnFleetValue");
-            CHANCE_TO_RESPAWN_WITH_NON_PLAYER_FACTION_FLEET_MULTIPLIER = (float) cfg.getDouble("chanceToRespawnWithNonPlayerFactionFleetMultiplier");
+            readSettings();
 
             return settingsAreRead = true;
         } catch (Exception e) {
